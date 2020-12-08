7c5debbf4e6879417d6cd3e86d4f1fdc73c131c5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-08 21:25:41 +0100

    #1 - Adjustment to model.

--- posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/model/Post.java
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/model/Post.java
@@ -1,7 +1,18 @@
 package com.github.starnowski.posmulten.demos.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import javax.persistence.*;
 
+@Data
+@Entity
+@Accessors(chain = true)
+@Table(name = "posts")
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Post {
 
     @Id
