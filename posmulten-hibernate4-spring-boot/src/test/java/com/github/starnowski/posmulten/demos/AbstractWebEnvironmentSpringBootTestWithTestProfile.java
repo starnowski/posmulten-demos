d80ff97ab510f0635a3dcce6b6beb8a1e061d915
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-09 22:40:20 +0100

    #1 - Added test class.

---
+++ posmulten-hibernate4-spring-boot/src/test/java/com/github/starnowski/posmulten/demos/AbstractWebEnvironmentSpringBootTestWithTestProfile.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.posmulten.demos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class AbstractWebEnvironmentSpringBootTestWithTestProfile {
+}
