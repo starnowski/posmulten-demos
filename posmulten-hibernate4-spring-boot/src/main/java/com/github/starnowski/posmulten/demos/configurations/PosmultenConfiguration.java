cc50623c4f41395c330479dc19883b0b207da60f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-09 23:29:01 +0100

    #1 - Added posmulten configuration.

---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/configurations/PosmultenConfiguration.java
@@ -0,0 +1,35 @@
+package com.github.starnowski.posmulten.demos.configurations;
+
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
+import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.ISetCurrentTenantIdFunctionInvocationFactory;
+import com.github.starnowski.posmulten.postgresql.core.rls.function.ISetCurrentTenantIdFunctionPreparedStatementInvocationFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PosmultenConfiguration {
+
+
+    @Bean
+    public ISharedSchemaContext iSharedSchemaContext() throws SharedSchemaContextBuilderException {
+        DefaultSharedSchemaContextBuilder defaultSharedSchemaContextBuilder = new DefaultSharedSchemaContextBuilder(null); // null schema --> public schema
+        defaultSharedSchemaContextBuilder.setGrantee("posmhib4sb-user"); // TODO move to configuration file
+        defaultSharedSchemaContextBuilder.setCurrentTenantIdProperty("posdemo.tenant"); // TODO move to configuration file
+        defaultSharedSchemaContextBuilder.setSetCurrentTenantIdFunctionName("set_pos_demo_tenant"); // TODO move to configuration file
+        return defaultSharedSchemaContextBuilder.build();
+    }
+
+    @Bean
+    public ISetCurrentTenantIdFunctionInvocationFactory iSetCurrentTenantIdFunctionInvocationFactory(ISharedSchemaContext iSharedSchemaContext)
+    {
+        return iSharedSchemaContext.getISetCurrentTenantIdFunctionInvocationFactory();
+    }
+
+    @Bean
+    public ISetCurrentTenantIdFunctionPreparedStatementInvocationFactory iSetCurrentTenantIdFunctionPreparedStatementInvocationFactory(ISharedSchemaContext iSharedSchemaContext)
+    {
+        return iSharedSchemaContext.getISetCurrentTenantIdFunctionPreparedStatementInvocationFactory();
+    }
+}
