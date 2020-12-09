ea7bde129f290aa913476fc79861965e3e62a4b6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-09 22:58:11 +0100

    #1 - Added test class.

---
+++ posmulten-hibernate4-spring-boot/src/test/java/com/github/starnowski/posmulten/demos/NativeSqlWithRlsTest.java
@@ -0,0 +1,129 @@
+package com.github.starnowski.posmulten.demos;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
+
+public class NativeSqlWithRlsTest extends AbstractWebEnvironmentSpringBootTestWithTestProfile {
+
+    @Autowired
+    protected JdbcTemplate jdbcTemplate;
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldReadRecordFromSameTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isEqualTo(1);
+
+        // when
+        int result = TestUtils.countNumberOfRecordsWhereByTenantId(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'", "xds");
+
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldNotAbleToReadRecordFromOtherTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isEqualTo(1);
+
+        // when
+        int result = TestUtils.countNumberOfRecordsWhereByTenantId(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'", "xds1");
+
+        // then
+        assertThat(result).isZero();
+    }
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldUpdateRecordForSameTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski'")).isEqualTo(1);
+
+        // when
+        jdbcTemplate.execute(statementSettingCurrentTenantVariable("xds") + "UPDATE user_info SET username = 'starnowski1' WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'");
+
+        // then
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski'")).isZero();
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski1'")).isEqualTo(1);
+    }
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldNotAbleToUpdateRecordFromOtherTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski'")).isEqualTo(1);
+
+        // when
+        jdbcTemplate.execute(statementSettingCurrentTenantVariable("xds1") + "UPDATE user_info SET username = 'starnowski1' WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'");
+
+        // then
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski'")).isEqualTo(1);
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds' AND username = 'starnowski1'")).isZero();
+    }
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldDeleteRecordForSameTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isEqualTo(1);
+
+        // when
+        jdbcTemplate.execute(statementSettingCurrentTenantVariable("xds") + "DELETE FROM user_info WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'");
+
+        // then
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isZero();
+    }
+
+    @Test
+    @Sql(value = {TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH},
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
+            config = @SqlConfig(transactionMode = ISOLATED),
+            executionPhase = AFTER_TEST_METHOD)
+    public void shouldNotAbleToDeleteRecordFromOtherTenant() {
+        // given
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isEqualTo(1);
+
+        // when
+        jdbcTemplate.execute(statementSettingCurrentTenantVariable("xds1") + "DELETE FROM user_info WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'");
+
+        // then
+        Assertions.assertThat(TestUtils.countNumberOfRecordsWhere(jdbcTemplate, "user_info", "user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11' AND tenant_id = 'xds'")).isEqualTo(1);
+    }
+}
