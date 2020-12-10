be2cd7b65b65e11a5aa7fc82d3aa6102bc709e27
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-10 01:02:48 +0100

    #1 - Code adjustments.

---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/Application.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.posmulten.demos;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
