805aeee62e7926f20fb700ecafdec7f32887c0db
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-13 20:45:21 +0100

    #1 - Code refactoring.

--- posmulten-hibernate4-spring-boot/src/test/resources/com/github/starnowski/posmulten/demos/clean-database.sql
+++ posmulten-hibernate4-spring-boot/src/test/resources/com/github/starnowski/posmulten/demos/clean-database.sql
@@ -4,4 +4,8 @@ DELETE FROM tenant_info CASCADE;
 DELETE FROM posts CASCADE;
 DELETE FROM comments CASCADE;
 
+--TODO Add option in builder component so that the correct access for grantee could be added.
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "posmhib4sb-user";
+
+
 
