57d9b250657345331f001ee943457063cb7468f1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-13 16:54:44 +0100

    #1 - Code refactoring.

--- posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/configurations/PosmultenConfiguration.java
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/configurations/PosmultenConfiguration.java
@@ -1,20 +1,22 @@
 package com.github.starnowski.posmulten.demos.configurations;
 
+import com.github.starnowski.posmulten.demos.hibernate.DefaultSharedSchemaContextBuilderFactory;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 import com.github.starnowski.posmulten.postgresql.core.context.exceptions.SharedSchemaContextBuilderException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PosmultenConfiguration {
 
+    @Autowired
+    private DefaultSharedSchemaContextBuilderFactory defaultSharedSchemaContextBuilderFactory;
+
     @Bean
     public ISharedSchemaContext iSharedSchemaContext() throws SharedSchemaContextBuilderException {
-        DefaultSharedSchemaContextBuilder defaultSharedSchemaContextBuilder = new DefaultSharedSchemaContextBuilder(null); // null schema --> public schema
-        defaultSharedSchemaContextBuilder.setGrantee("posmhib4sb-user"); // TODO move to configuration file
-        defaultSharedSchemaContextBuilder.setCurrentTenantIdProperty("posdemo.tenant"); // TODO move to configuration file
-        defaultSharedSchemaContextBuilder.setSetCurrentTenantIdFunctionName("set_pos_demo_tenant"); // TODO move to configuration file
+        DefaultSharedSchemaContextBuilder defaultSharedSchemaContextBuilder = defaultSharedSchemaContextBuilderFactory.build();
         return defaultSharedSchemaContextBuilder.build();
     }
 }
---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/hibernate/DefaultSharedSchemaContextBuilderFactory.java
@@ -0,0 +1,17 @@
+package com.github.starnowski.posmulten.demos.hibernate;
+
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultSharedSchemaContextBuilderFactory {
+
+    public DefaultSharedSchemaContextBuilder build()
+    {
+        DefaultSharedSchemaContextBuilder defaultSharedSchemaContextBuilder = new DefaultSharedSchemaContextBuilder(null); // null schema --> public schema
+        defaultSharedSchemaContextBuilder.setGrantee("posmhib4sb-user"); // TODO move to configuration file
+        defaultSharedSchemaContextBuilder.setCurrentTenantIdProperty("posdemo.tenant"); // TODO move to configuration file
+        defaultSharedSchemaContextBuilder.setSetCurrentTenantIdFunctionName("set_pos_demo_tenant"); // TODO move to configuration file
+        return defaultSharedSchemaContextBuilder;
+    }
+}
