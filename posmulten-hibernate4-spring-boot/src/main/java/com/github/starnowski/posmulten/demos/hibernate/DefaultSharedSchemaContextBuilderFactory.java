0934c09aec5afca72ea7639e5bfe9c214e97b5c1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-13 21:50:50 +0100

    #1 - Code refactoring.

--- posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/hibernate/DefaultSharedSchemaContextBuilderFactory.java
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/hibernate/DefaultSharedSchemaContextBuilderFactory.java
@@ -3,6 +3,9 @@ package com.github.starnowski.posmulten.demos.hibernate;
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder;
 import org.springframework.stereotype.Component;
 
+import static com.github.starnowski.posmulten.demos.util.TenantContext.INVALID_TENANT_ID;
+import static java.util.Collections.singletonList;
+
 @Component
 public class DefaultSharedSchemaContextBuilderFactory {
 
@@ -13,6 +16,7 @@ public class DefaultSharedSchemaContextBuilderFactory {
         defaultSharedSchemaContextBuilder.setCurrentTenantIdProperty("posdemo.tenant"); // TODO move to configuration file
         defaultSharedSchemaContextBuilder.setSetCurrentTenantIdFunctionName("set_pos_demo_tenant"); // TODO move to configuration file
         defaultSharedSchemaContextBuilder.setCurrentTenantIdentifierAsDefaultValueForTenantColumnInAllTables(true); // TODO move to configuration file
+        defaultSharedSchemaContextBuilder.createValidTenantValueConstraint(singletonList(INVALID_TENANT_ID), null, null); // TODO move to configuration file
         return defaultSharedSchemaContextBuilder;
     }
 }
---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/util/TenantContext.java
@@ -0,0 +1,15 @@
+package com.github.starnowski.posmulten.demos.util;
+
+public class TenantContext {
+
+    public static final String INVALID_TENANT_ID = "invalid_tenant_id";
+    private static ThreadLocal<String> currentTenant = ThreadLocal.withInitial(() -> INVALID_TENANT_ID);
+
+    public static String getCurrentTenant() {
+        return currentTenant.get();
+    }
+
+    public static void setCurrentTenant(String tenant) {
+        currentTenant.set(tenant);
+    }
+}
