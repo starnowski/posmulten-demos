07b99c8b2c1bb54d1cf7bad8a574a35757b8dc9f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-15 01:00:29 +0100

    #1 - Code refactoring.

---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.starnowski.posmulten.demos.dao;
+
+import com.github.starnowski.posmulten.demos.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
