2314fb9c7aedbb28b8ebdaa004f30c51e07254ee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-16 02:31:29 +0100

    #1 - Added tenant rest api controller (not yet implemented).

---
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/controllers/TenantController.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posmulten.demos.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@RequestMapping("/app/tenants")
+public class TenantController {
+}
