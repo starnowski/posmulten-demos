02b94e4761387492e7d40ce8718d5a0baac0fa42
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-16 20:33:25 +0100

    #1 - Added tests classes.

---
+++ posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/SpecificationWithSpringBootWebEnvironmentTestContext.groovy
@@ -0,0 +1,12 @@
+package com.github.starnowski.posmulten.demos
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class SpecificationWithSpringBootWebEnvironmentTestContext extends Specification{
+}
---
+++ posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/controllers/TenantControllerTest.groovy
@@ -0,0 +1,6 @@
+package com.github.starnowski.posmulten.demos.controllers
+
+import spock.lang.Specification
+
+class TenantControllerTest extends Specification {
+}
