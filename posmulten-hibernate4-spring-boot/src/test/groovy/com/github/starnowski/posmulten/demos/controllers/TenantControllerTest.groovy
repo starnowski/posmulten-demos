4396c9a629c2f20332bd0596a00c110e42d124ea
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-17 00:59:20 +0100

    #1 - Code adjustments.

--- posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/controllers/TenantControllerTest.groovy
+++ posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/controllers/TenantControllerTest.groovy
@@ -1,5 +1,6 @@
 package com.github.starnowski.posmulten.demos.controllers
 
+import com.github.starnowski.posmulten.demos.SpecificationWithSpringBootWebEnvironmentTestContext
 import com.github.starnowski.posmulten.demos.TestUtils
 import com.github.starnowski.posmulten.demos.dto.TenantDto
 import org.springframework.beans.factory.annotation.Autowired
@@ -7,7 +8,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlConfig
-import spock.lang.Specification
+import org.springframework.test.context.jdbc.SqlGroup
 import spock.lang.Unroll
 
 import static com.github.starnowski.posmulten.demos.configurations.OwnerDataSourceConfiguration.OWNER_TRANSACTION_MANAGER
@@ -15,13 +16,14 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TES
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED
 
+@SqlGroup([
 @Sql(value = [ TestUtils.GRANT_ACCESS_TO_DB_USER_SCRIPT_PATH, TestUtils.CLEAR_DATABASE_SCRIPT_PATH, TestUtils.TEST_BASIC_DATA_SCRIPT_PATH],
         config = @SqlConfig(transactionMode = ISOLATED, dataSource = "ownerDataSource", transactionManager = OWNER_TRANSACTION_MANAGER),
-        executionPhase = BEFORE_TEST_METHOD)
+        executionPhase = BEFORE_TEST_METHOD),
 @Sql(value = TestUtils.CLEAR_DATABASE_SCRIPT_PATH,
         config = @SqlConfig(transactionMode = ISOLATED, dataSource = "ownerDataSource", transactionManager = OWNER_TRANSACTION_MANAGER),
-        executionPhase = AFTER_TEST_METHOD)
-class TenantControllerTest extends Specification {
+        executionPhase = AFTER_TEST_METHOD)])
+class TenantControllerTest extends SpecificationWithSpringBootWebEnvironmentTestContext {
 
     @Autowired
     TestRestTemplate restTemplate
@@ -34,15 +36,15 @@ class TenantControllerTest extends Specification {
     @Unroll
     def "should modify company data base on request body"()
     {
-            given:
+        given:
             TenantDto dto = new TenantDto()
             dto.setName(tenant)
 
         when:
-            def TenantDto = restTemplate.postForEntity("/app/tenants", dto, TenantDto.class)
+            def result = restTemplate.postForEntity("/app/tenants", dto, TenantDto.class)
 
         then:
-            dto.name == tenant
+            result.name == tenant
 
         where:
             tenant << ["xdds", "ten1", "some_com_ten"]
