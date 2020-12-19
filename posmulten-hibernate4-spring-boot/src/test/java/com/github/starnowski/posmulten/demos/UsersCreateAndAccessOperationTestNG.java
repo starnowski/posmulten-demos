9008c906aa621614bdfea173e073fbfb85913cb0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-19 21:50:55 +0100

    #1 - Code adjustments.

--- posmulten-hibernate4-spring-boot/src/test/java/com/github/starnowski/posmulten/demos/UsersCreateAndAccessOperationTestNG.java
+++ posmulten-hibernate4-spring-boot/src/test/java/com/github/starnowski/posmulten/demos/UsersCreateAndAccessOperationTestNG.java
@@ -4,6 +4,8 @@ import com.github.starnowski.posmulten.demos.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.testng.annotations.BeforeMethod;
@@ -62,13 +64,34 @@ public class UsersCreateAndAccessOperationTestNG extends TestNGSpringContextWith
         String url = appTenantUrl(tenant, "users");
 
         // when
-        restTemplate.postForEntity(url, user, UserDto.class);
+        ResponseEntity<UserDto> response = restTemplate.postForEntity(url, user, UserDto.class);
 
         // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(countUsersWithSpecifiedName(user.getUsername(), tenant)).isEqualTo(1);
     }
 
-    @Test(dependsOnMethods = "createUser", alwaysRun = true)
+    @Test(dependsOnMethods = "createUser", dataProvider = "userData")
+    public void loginWithBasicWhileAddingPostsResources(UserDto user, String tenant)
+    {
+        // given
+        String url = appTenantUrl(tenant, "posts");
+
+        // when
+        //TODO
+    }
+
+    @Test(dependsOnMethods = "createUser", dataProvider = "userData")
+    public void loginWithBasicWhileReadingAllTenantPostsResources(UserDto user, String tenant)
+    {
+        // given
+        String url = appTenantUrl(tenant, "posts");
+
+        // when
+
+    }
+
+    @Test(dependsOnMethods = {"createUser", "loginWithBasicWhileReadingAllTenantPostsResources"}, alwaysRun = true)
     @Sql(value = {CLEAR_DATABASE_SCRIPT_PATH},
             config = @SqlConfig(transactionMode = ISOLATED, dataSource = OWNER_DATA_SOURCE, transactionManager = OWNER_TRANSACTION_MANAGER),
             executionPhase = BEFORE_TEST_METHOD)
