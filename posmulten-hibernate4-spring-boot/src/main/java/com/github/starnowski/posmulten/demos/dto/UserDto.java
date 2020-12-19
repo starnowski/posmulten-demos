ecfb21c62dc21c9608a956a114b5c02e2e9ae180
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-19 13:39:17 +0100

    #1 - Added tests.

--- posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/dto/UserDto.java
+++ posmulten-hibernate4-spring-boot/src/main/java/com/github/starnowski/posmulten/demos/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.github.starnowski.posmulten.demos.dto;
 import com.github.starnowski.posmulten.demos.util.RoleEnum;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 import java.util.Set;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Data
 @Accessors(chain = true)
 @EqualsAndHashCode(of = "userId")
+@ToString
 public class UserDto {
 
     private UUID userId;
--- posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/controllers/UsersControllerTest.groovy
+++ posmulten-hibernate4-spring-boot/src/test/groovy/com/github/starnowski/posmulten/demos/controllers/UsersControllerTest.groovy
@@ -2,11 +2,20 @@ package com.github.starnowski.posmulten.demos.controllers
 
 import com.github.starnowski.posmulten.demos.SpecificationWithSpringBootWebEnvironmentTestContext
 import com.github.starnowski.posmulten.demos.TestUtils
+import com.github.starnowski.posmulten.demos.dto.TenantDto
+import com.github.starnowski.posmulten.demos.dto.UserDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlConfig
 import org.springframework.test.context.jdbc.SqlGroup
+import spock.lang.Unroll
 
+import static com.github.starnowski.posmulten.demos.TestUtils.countNumberOfRecordsWhere
 import static com.github.starnowski.posmulten.demos.configurations.OwnerDataSourceConfiguration.OWNER_TRANSACTION_MANAGER
+import static org.assertj.core.api.Assertions.assertThat
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED
@@ -19,4 +28,37 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
                 config = @SqlConfig(transactionMode = ISOLATED, dataSource = "ownerDataSource", transactionManager = OWNER_TRANSACTION_MANAGER),
                 executionPhase = AFTER_TEST_METHOD)])
 class UsersControllerTest extends SpecificationWithSpringBootWebEnvironmentTestContext {
+
+    @Autowired
+    TestRestTemplate restTemplate
+
+    @Autowired
+    @Qualifier("ownerJdbcTemplate")
+    protected JdbcTemplate ownerJdbcTemplate
+
+    @Unroll
+    def "should create user (#user) based on request body"()
+    {
+        given:
+            assertThat(countUsersWithSpecifiedName(user.getUsername())).isZero()
+
+        when:
+            def result = restTemplate.postForEntity("/app/users", user, TenantDto.class)
+
+        then:
+            result.body.username == user.getUsername()
+
+        and: "number of added users should be correct"
+            countUsersWithSpecifiedName(user.getUsername()) == 1
+
+        where:
+            user                                                    |   tenant
+            new UserDto().setUsername("John").setPassword("pass1")  |   "ten1"
+            new UserDto().setUsername("Bill").setPassword("pass1")  |   "xds"
+    }
+
+    int countUsersWithSpecifiedName(String name)
+    {
+        countNumberOfRecordsWhere(ownerJdbcTemplate, "user_info", "username = '" + name + "'")
+    }
 }
