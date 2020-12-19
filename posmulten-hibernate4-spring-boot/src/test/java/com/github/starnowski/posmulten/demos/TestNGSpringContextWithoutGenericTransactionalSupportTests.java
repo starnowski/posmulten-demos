326eae07e6f021b843acd9e8a0ea4320a1b1d9d5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-19 17:40:35 +0100

    #1 - Added testng tests class.

---
+++ posmulten-hibernate4-spring-boot/src/test/java/com/github/starnowski/posmulten/demos/TestNGSpringContextWithoutGenericTransactionalSupportTests.java
@@ -0,0 +1,11 @@
+package com.github.starnowski.posmulten.demos;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = Application.class)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+public class TestNGSpringContextWithoutGenericTransactionalSupportTests extends AbstractTransactionalTestNGSpringContextTests {
+}
