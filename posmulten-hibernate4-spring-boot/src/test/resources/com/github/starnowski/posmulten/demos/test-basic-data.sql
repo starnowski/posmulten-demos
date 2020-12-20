2a8a9b84e3dc19ec873d68ea9be0b806fc647e54
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-12-20 03:45:53 +0100

    #1 - Refactoring.

--- posmulten-hibernate4-spring-boot/src/test/resources/com/github/starnowski/posmulten/demos/test-basic-data.sql
+++ posmulten-hibernate4-spring-boot/src/test/resources/com/github/starnowski/posmulten/demos/test-basic-data.sql
@@ -1,8 +1,8 @@
 --- tenant xds1
 SELECT set_pos_demo_tenant('xds');
 INSERT INTO user_info (user_id, username, tenant_id) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'starnowski', 'xds');
-INSERT INTO user_info (user_id, username, tenant_id) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'mkyc', 'xds');
+INSERT INTO user_info (user_id, username, tenant_id) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'anonim', 'xds');
 
 --- tenant xds1
 SELECT set_pos_demo_tenant('xds1');
-INSERT INTO user_info (user_id, username, tenant_id) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'kglenny', 'xds1');
+INSERT INTO user_info (user_id, username, tenant_id) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'columb', 'xds1');
