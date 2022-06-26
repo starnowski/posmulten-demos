8211ebaa7824c10d06234cc5bea733adef76f8b6
starnowski <33316705+starnowski@users.noreply.github.com>
2022-06-26 11:42:16 +0200

    #8 Code refactoring [skip ci]

---
+++ posmulten-hibernate5-spring-boot-thymeleaf/src/main/java/com/github/starnowski/posmulten/demos/posmultenhibernate5springbootthymeleaf/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.github.starnowski.posmulten.demos.posmultenhibernate5springbootthymeleaf.repositories;
+
+import com.github.starnowski.posmulten.demos.posmultenhibernate5springbootthymeleaf.model.TenantInfo;
+import com.github.starnowski.posmulten.demos.posmultenhibernate5springbootthymeleaf.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+
+}
