79cb1f9451b113c37dbd6df75a129b673bf53251
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-07-09 01:08:57 +0200

    #8 Fixed layout

--- posmulten-hibernate5-spring-boot-thymeleaf/src/main/java/com/github/starnowski/posmulten/demos/posmultenhibernate5springbootthymeleaf/configurations/ThymeleafConfiguration.java
+++ posmulten-hibernate5-spring-boot-thymeleaf/src/main/java/com/github/starnowski/posmulten/demos/posmultenhibernate5springbootthymeleaf/configurations/ThymeleafConfiguration.java
@@ -1,11 +1,11 @@
 package com.github.starnowski.posmulten.demos.posmultenhibernate5springbootthymeleaf.configurations;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
@@ -27,26 +27,31 @@ public class ThymeleafConfiguration {
         templateResolver.setCacheable(true);
         return templateResolver;
     }
-
-    @Bean
-    public SpringTemplateEngine templateEngine(@Autowired ApplicationContext applicationContext){
-        // SpringTemplateEngine automatically applies SpringStandardDialect and
-        // enables Spring's own MessageSource message resolution mechanisms.
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver(applicationContext));
-        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-        // speed up execution in most scenarios, but might be incompatible
-        // with specific cases when expressions in one template are reused
-        // across different data types, so this flag is "false" by default
-        // for safer backwards compatibility.
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
+//
+//    @Bean
+//    public SpringTemplateEngine templateEngine(@Autowired ApplicationContext applicationContext){
+//        // SpringTemplateEngine automatically applies SpringStandardDialect and
+//        // enables Spring's own MessageSource message resolution mechanisms.
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(templateResolver(applicationContext));
+//        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+//        // speed up execution in most scenarios, but might be incompatible
+//        // with specific cases when expressions in one template are reused
+//        // across different data types, so this flag is "false" by default
+//        // for safer backwards compatibility.
+//        templateEngine.setEnableSpringELCompiler(true);
+//        return templateEngine;
+//    }
 
     @Bean
     public ResourceBundleMessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("Messages");
         return messageSource;
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 }
