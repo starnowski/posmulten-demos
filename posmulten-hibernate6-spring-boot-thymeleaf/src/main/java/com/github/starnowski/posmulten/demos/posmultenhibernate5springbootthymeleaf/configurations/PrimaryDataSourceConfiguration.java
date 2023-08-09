eedc8cc62542444f28a7f1ea7cb85109bb74de98
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-08-09 18:52:32 +0200

    #16 - Update version of posmulten-hibernate to 0.2.2

--- posmulten-hibernate6-spring-boot-thymeleaf/src/main/java/com/github/starnowski/posmulten/demos/posmultenhibernate5springbootthymeleaf/configurations/PrimaryDataSourceConfiguration.java
+++ posmulten-hibernate6-spring-boot-thymeleaf/src/main/java/com/github/starnowski/posmulten/demos/posmultenhibernate5springbootthymeleaf/configurations/PrimaryDataSourceConfiguration.java
@@ -100,8 +100,6 @@ public class PrimaryDataSourceConfiguration {
                 "hibernate.connection.username", primaryDataSourceProperties().getUsername());
         hibernateProperties.setProperty(
                 "hibernate.connection.password", primaryDataSourceProperties().getPassword());
-        //org.hibernate.HibernateException: The internal connection pool has reached its maximum size and no connection is currently available!
-        hibernateProperties.put("hibernate.connection.pool_size", "600");//TODO Lower
         return hibernateProperties;
     }
 
